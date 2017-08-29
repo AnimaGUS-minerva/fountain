dabe42187a1f35c48f34a6f6d94f2f703f6773c1
Michael Richardson <mcr@sandelman.ca>
2017-08-29 15:46:40 -0400

    only keep requestvoucher route test

--- spec/routing/est_routing_spec.rb
+++ spec/routing/est_routing_spec.rb
@@ -3,26 +3,8 @@ require "rails_helper"
 RSpec.describe EstController, type: :routing do
   describe "routing" do
 
-
-    it "routes to #show" do
-      expect(:get => "/ests/1").to route_to("ests#show", :id => "1")
-    end
-
-
-    it "routes to #create" do
-      expect(:post => "/ests").to route_to("ests#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/ests/1").to route_to("ests#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/ests/1").to route_to("ests#update", :id => "1")
-    end
-
-    it "routes to #destroy" do
-      expect(:delete => "/ests/1").to route_to("ests#destroy", :id => "1")
+    it "should map /.well-known/est/requestvoucher to #requestvoucher" do
+      expect(:post => "/.well-known/est/requestvoucher").to route_to("est#requestvoucher")
     end
 
   end
