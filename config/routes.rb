25b129fa205a5ddd90ca0288a7a811e3706289a0
Michael Richardson <mcr@sandelman.ca>
2017-11-14 00:48:07 -0500

    include admin interface only when testing, or when $ADMININTERFACE defined

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
-  resources :vouchers do
-    as_routes
-  end
-  resources :voucher_requests do
-    as_routes
-  end
-  resources :device_types do
-    as_routes
-  end
-  resources :manufacturers do
-    as_routes
-  end
-  resources :nodes do
-    as_routes
-  end
-  resources :certificates do
-    as_routes
+  if $ADMININTERFACE or Rails.env == 'test'
+    concern :active_scaffold
+    concern :active_scaffold_association, ActiveScaffold::Routing::Association.new
+    concern :active_scaffold, ActiveScaffold::Routing::Basic.new(association: true)
+
+    resources :vouchers, concerts: :active_scaffold
+    resources :voucher_requests, concerts: :active_scaffold
+    resources :device_types, concerts: :active_scaffold
+    resources :manufacturers, concerts: :active_scaffold
+    resources :nodes, concerts: :active_scaffold
+    resources :certificates, concerts: :active_scaffold
   end
 
   # EST processing at well known URLs
