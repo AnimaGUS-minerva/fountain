3a551cf57ff1b0dc23abd95ac53c6785d16dd301
Michael Richardson <mcr@sandelman.ca>
2018-05-16 22:47:06 -0400

    example voucher input and output for voucher_spec tests

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -16,8 +16,28 @@ RSpec.describe Voucher, type: :model do
   end
 
   describe "receiving vouchers" do
+    it "should read a sample voucher from a file" do
+      voucher_binary=IO::read(File.join("spec","files","voucher_jada123456789.vch"))
+
+      v1 = CoseVoucher.from_voucher(:cose, voucher_binary)
+      expect(v1).to proximity
+      expect(v1.serialNumber).to eq('JADA123456789')
+      expect(v1.nonce).to        eq('abcd12345')
+    end
+
+
     it "should create voucher object and place the signed data in it" do
       voucher_base64 = IO::read(File.join("spec","files","voucher_JADA_f2-00-01.pkcs"))
+      voucher_binary = Base64.decode64(voucher_base64)
+
+      v1 = CmsVoucher.from_voucher(:pkcs7, voucher_binary)
+
+      expect(v1.node).to eq(nodes(:jadaf20001))
+      expect(v1).to proximity
+    end
+
+    it "should create constrained voucher object and place the signed data in it" do
+      voucher_base64 = IO::read(File.join("spec","files","voucher_00-D0-E5-01-00-09.vch"))
       voucher_binary = Base64.decode64(voucher_base64)
 
       v1 = CmsVoucher.from_voucher(:pkcs7, voucher_binary)
