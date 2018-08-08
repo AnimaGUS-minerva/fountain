e87ddd0699863a6213cb4ebba4cedd28b5c6ece6
Michael Richardson <mcr@sandelman.ca>
2018-08-08 17:18:41 -0400

    protect HTTPS uses of fountain from annotations due to David/COAP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # David (COAP server), adds discoverable option
+  unless self.respond_to?(:discoverable)
+    def self.discoverable(options)
+      # true
+    end
+  end
+
 end
