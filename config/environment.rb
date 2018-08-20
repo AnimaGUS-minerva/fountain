70c4665a43b28af6c9242b409a06b91fa38bb18e
Michael Richardson <mcr@sandelman.ca>
2018-08-20 01:02:57 -0400

    let sqlite3 store booleans as integers

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
 # Initialize the Rails application.
 Rails.application.initialize!
+
