f78ae57c175d32d66e4166d5470f909374176bca
Michael Richardson <mcr@sandelman.ca>
2018-08-28 15:50:53 -0400

    validate the signer of the mudfile

--- app/models/device_type.rb
+++ app/models/device_type.rb
@@ -1,8 +1,11 @@
 require 'open-uri'
+require 'digest'
 
 class DeviceType < ActiveRecord::Base
   has_many :devices
   belongs_to :manufacturer
+
+  attr_accessor :raw_json
 
   def self.find_or_create_by_mud_url(mu)
     dt = find_by_mud_url(mu)
@@ -27,16 +30,38 @@ class DeviceType < ActiveRecord::Base
   end
 
   def validate_mud_url
+    signature = nil
     open(mud_url_sig) { |f|
       signature = f.read
     }
 
+    # empty certificate store.
+    cert_store    = OpenSSL::X509::Store.new
+    pkcs7activity = OpenSSL::PKCS7.new(signature)
+    certlist      = []
+    certs         = pkcs7activity.certificates
+    if certs
+      sign0         = certs.first
+      certlist      = [sign0]
+    end
+
+    # do not attempt to validate the certificates.
+    flags         = OpenSSL::PKCS7::NOCHAIN|OpenSSL::PKCS7::NOVERIFY
+    result        = pkcs7activity.verify(certlist, cert_store, raw_json, flags)
+
+    self.validated_mud_json = mud_json
+
+    # find manufacturer by signer of mudfile.
+
+    # do something if result == false?
+    result
   end
 
   def mud_json
     unless @mud_json.kind_of? Hash
       open(mud_url) {|f|
-        self.mud_json = JSON::parse(f.read)
+        @raw_json = f.read
+        self.mud_json = JSON::parse(@raw_json)
       }
     end
     @mud_json
