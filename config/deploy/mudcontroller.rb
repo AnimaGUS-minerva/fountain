236d830edfe7ddf9bc4923bcd8eedac9a326aa8b
Michael Richardson <mcr@sandelman.ca>
2018-09-06 16:12:19 -0400

    move to ruby version 2.5.1 for mud-controller LXC

--- config/deploy/mudcontroller.rb
+++ config/deploy/mudcontroller.rb
@@ -3,11 +3,12 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "mud-controller", user: "mud", roles: %{app db web}
+#server "mud-controller", user: "mud", roles: %{app db web}
+server "heramud",        user: "mud", roles: %{app db web}
 
 before 'deploy:setup', 'rvm:install_rvm'  # install/update RVM
 before 'deploy:setup', 'rvm:install_ruby' # install Ruby and create gemset, OR:
 
 set :rvm_type, :system
-set :rvm_ruby_version, '2.4.1'
+set :rvm_ruby_version, '2.5.1'
 
