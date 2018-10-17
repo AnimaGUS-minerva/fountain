c70d038ca5edd0f0dfe09103035f679672ee683e
Michael Richardson <mcr@sandelman.ca>
2018-10-17 11:27:21 -0400

    include deployment instructions for eeylops testing host

---
+++ config/deploy/eeylops.rb
@@ -0,0 +1,14 @@
+# server-based syntax
+# ======================
+# Defines a single server with a list of roles and multiple properties.
+# You can define all roles on a single server, or split them:
+
+server "eeylops", user: "fountain", roles: %{app db web}
+
+#before 'deploy:setup', 'rvm:install_rvm'  # install/update RVM
+#before 'deploy:setup', 'rvm:install_ruby' # install Ruby and create gemset, OR:
+
+set :rvm_custom_path, '/usr/share/rvm'
+set :rvm_type, :system
+set :rvm_ruby_version, '2.5.1'
+
