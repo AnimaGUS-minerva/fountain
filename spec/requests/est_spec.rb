0ad8765fd3b683c50ca0559f4f2bb7996a5168b3
Michael Richardson <mcr@sandelman.ca>
2018-10-23 23:27:53 -0400

    added a second constrained voucher example that reaches out to highway-test

--- spec/requests/est_spec.rb
+++ spec/requests/est_spec.rb
@@ -40,6 +40,10 @@ RSpec.describe "Est", type: :request do
     @cbor_clientcert ||= IO.binread("spec/certs/F21003-idevid.pem")
   end
 
+  def cbor_highwaytest_clientcert
+    @cbor_highwaytest_clientcert ||= IO.binread("spec/certs/E0000F-idevid.pem")
+  end
+
   describe "resource discovery" do
     it "should return a location for the EST service" do
       env = Hash.new
@@ -164,6 +168,48 @@ RSpec.describe "Est", type: :request do
       vr0 = Chariwt::Voucher.from_cbor_cose(voucher_request, FountainKeys.ca.jrc_pub_key)
       expect(vr0).to_not be_nil
 
+      expect(Chariwt.cmp_vch_file(voucher_request,
+                                  "parboiled_vr_00-D0-E5-F2-10-03")).to be true
+
+      expect(Chariwt.cmp_vch_file(assigns(:voucher).signed_voucher,
+                                  "voucher_00-D0-E5-F2-10-03")).to be true
+
+      expect(Chariwt.cmp_vch_file(response.body,
+                                  "voucher_00-D0-E5-F2-10-03")).to be true
+
+    end
+
+    it "should get CoAPS POSTed to cbor_rv, onwards to highway-test" do
+      voucher_request = nil
+      @time_now = Time.at(1507671037)  # Oct 10 17:30:44 EDT 2017
+      allow(Time).to receive(:now).and_return(@time_now)
+
+      WebMock.allow_net_connect!
+
+      # get the Base64 of the incoming signed request
+      body = IO.read("spec/files/vr_00-D0-E5-E0-00-0F.vch")
+
+      env = Hash.new
+      env["SSL_CLIENT_CERT"] = cbor_highwaytest_clientcert
+      env["HTTP_ACCEPT"]  = "application/voucher-cose+cbor"
+      env["CONTENT_TYPE"] = "application/voucher-cose+cbor"
+
+      $FAKED_TEMPORARY_KEY = temporary_key
+      post '/e/rv', :params => body, :headers => env
+
+      expect(assigns(:voucherreq)).to_not be_nil
+      expect(assigns(:voucherreq).tls_clientcert).to_not be_nil
+      expect(assigns(:voucherreq).pledge_request).to_not be_nil
+      expect(assigns(:voucherreq).signed).to be_truthy
+      expect(assigns(:voucherreq).node).to_not be_nil
+      expect(assigns(:voucherreq).manufacturer).to be_present
+      expect(assigns(:voucherreq).device_identifier).to_not be_nil
+
+      # validate that the voucher_request can be validated with the key used.
+      vr0 = Chariwt::Voucher.from_cbor_cose(voucher_request, FountainKeys.ca.jrc_pub_key)
+      expect(vr0).to_not be_nil
+
+      pending "do not yet have results from highway"
       expect(Chariwt.cmp_vch_file(voucher_request,
                                   "parboiled_vr_00-D0-E5-F2-10-03")).to be true
 
