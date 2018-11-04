53e2391e4d03fb00c305d25f24dbef496446f548
Michael Richardson <mcr@sandelman.ca>
2018-11-04 01:23:40 -0400

    move setting of eager_load_paths to application.rb

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Fountain
 
     config.autoload_paths << Rails.root.join('lib')
 
+    # config.autoload_paths << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
+
   end
 end
