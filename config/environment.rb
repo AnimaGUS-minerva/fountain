8d65b2def84bc3118eb74470e809437600c89278
Michael Richardson <mcr@sandelman.ca>
2018-11-19 18:56:21 -0500

    install custom mime types, set the version and revision for /version API call

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+$VERSION = "0.5"
+$ADMININTERFACE = false
+
+# gets overritten by config/initializers/revision.rb by capistrano
+$REVISION= "devel"
+
+Mime::Type.register "application/voucher-cose+cbor", :vcc
+Mime::Type.register "application/pkcs7-mime", :cms
+Mime::Type.register "application/cms",        :cms
+
