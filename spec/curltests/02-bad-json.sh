4f6d83539589f1422e0263ed7b2bb0d973e195ec
Michael Richardson <mcr@sandelman.ca>
2018-11-19 21:24:56 -0500

    fix bad-json curl test to work with fountain

--- spec/curltests/02-bad-json.sh
+++ spec/curltests/02-bad-json.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl --cacert db/cert/vendor_secp384r1.crt -X POST -H "Accept: */*" -H "Content-Type: application/json" --data-binary @spec/files/raw_unsigned_vr-00-12-34-56-78-9A.json https://highway-test.example.com/.well-known/est/requestvoucher
+curl --cacert db/cert/ownerca_secp384r1.crt -X POST -H "Accept: */*" -H "Content-Type: application/json" --data-binary @spec/files/raw_unsigned_vr-00-12-34-56-78-9A.json https://fountain-test.example.com/.well-known/est/requestvoucher
+
+
