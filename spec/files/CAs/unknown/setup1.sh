616123bc18e1ed44fc3c8eef594d28da37dfca51
Michael Richardson <mcr@sandelman.ca>
2018-12-11 18:00:35 -0500

    wo#14589 . generate root certificate for "unknown" manufacturer

---
+++ spec/files/CAs/unknown/setup1.sh
@@ -0,0 +1,22 @@
+export dir=`pwd`
+export cadir=`pwd`
+export format=pem
+mkdir -p $dir
+cd $dir
+mkdir -p certs crl csr newcerts private
+chmod 700 private
+touch index.txt
+touch serial
+sn=8
+
+countryName="/C=CA"
+stateOrProvinceName="/ST=Ontario"
+localityName="/L=Almonte"
+organizationName="/O=Fire Department"
+#organizationalUnitName="/OU="
+organizationalUnitName=
+commonName="/CN=Root CA"
+DN=$countryName$stateOrProvinceName$localityName
+DN=$DN$organizationName$organizationalUnitName$commonName
+echo $DN
+export subjectAltName=email:postmaster@unknownca.example.com
