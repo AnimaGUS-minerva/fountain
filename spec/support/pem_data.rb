35ae1d1ba4591e2e88a3f41dfe0ea44f3fbca374
Michael Richardson <mcr@sandelman.ca>
2019-01-11 17:47:00 -0500

    added second product from known manufacturer so that it can be found

--- spec/support/pem_data.rb
+++ spec/support/pem_data.rb
@@ -1,4 +1,10 @@
-# 00-D0-E5-02-00-20 is signed by manufacturer #2.
+# 00-D0-E5-02-00-20 is signed by manufacturer #2 (honeydukes)
 def cert1
   @cert1 ||= OpenSSL::X509::Certificate.new(File.read("spec/certs/00-D0-E5-02-00-20.crt"))
 end
+
+# 00-D0-E5-02-00-1B is signed by manufacturer #2 (honeydukes), but has no fixture
+def cert2
+  @cert2 ||= OpenSSL::X509::Certificate.new(File.read("spec/files/product_00-D0-E5-02-00-1B/device.crt"))
+end
+
