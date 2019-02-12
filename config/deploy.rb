2431c1fa10ba410e2e4d8b1f81b5b55c3ae624b4
Michael Richardson <mcr@sandelman.ca>
2019-02-11 19:31:00 -0500

    updated cap to 3.11

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.2"
+lock "~> 3.11.0"
 
 set :application, "fountain"
 set :repo_url, "http://github.com/AnimaGUS-minerva/fountain.git"
