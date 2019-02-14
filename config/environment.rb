93c178d93a260140bf345ec91300550aa41d5252
Michael Richardson <mcr@sandelman.ca>
2019-02-14 13:13:55 -0500

    bumped version to 0.6

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = tr
 # Initialize the Rails application.
 Rails.application.initialize!
 
-$VERSION = "0.5"
+$VERSION = "0.6"
 $ADMININTERFACE = false
 
 # gets overritten by config/initializers/revision.rb by capistrano
