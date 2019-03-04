2cb557990bc8de6a0fdd7dbe36887b8450e7cb9a
Michael Richardson <mcr@sandelman.ca>
2019-03-04 11:25:01 -0500

    setup the database correctly, and set up the entry point script for mud-supervisor use

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-echo starting
+if [ -z "$MY_RUBY_HOME" ]; then
+    . /etc/profile.d/rvm.sh
+    rvm use 2.6.1 >/dev/null
+fi
+
+export CERTDIR=/app/certificates
+export SERVCERT=${CERTDIR}/jrc_prime256v1.crt
+export SERVKEY=${CERTDIR}/jrc_prime256v1.key
+
+RAILS_ENV=${RAILS_ENV-production}
+export RAILS_ENV
+
+bundle exec thin start --ssl \
+       --servers 2 \
+       --port    8081  \
+       --user    fountain \
+       --ssl-cert-file ${SERVCERT} \
+       --ssl-key-file  ${SERVKEY} $@
+
+echo DONE
+
+
+
