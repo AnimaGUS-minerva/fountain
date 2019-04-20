059f24da678125cfcbd17788da01e6a384b73ee2
Michael Richardson <mcr@sandelman.ca>
2019-04-20 17:22:25 -0400

    attempt to deal with MASA returning 404 on constrained voucher request

--- spec/requests/est_spec.rb
+++ spec/requests/est_spec.rb
@@ -519,6 +519,39 @@ RSpec.describe "Est", type: :request do
 
     end
 
+    it "should get CoAPS POSTed to cbor_rv, and cope with 404 error from MASA" do
+      voucher_request = nil
+      @time_now = Time.at(1507671037)  # Oct 10 17:30:44 EDT 2017
+      allow(Time).to receive(:now).and_return(@time_now)
+
+      # get the Base64 of the incoming signed request
+      body = IO.read("spec/files/vr_00-D0-E5-F2-00-03.vrq")
+
+      env = Hash.new
+      env["SSL_CLIENT_CERT"] = cbor_clientcert_03
+      env["HTTP_ACCEPT"]  = "application/voucher-cose+cbor"
+      env["CONTENT_TYPE"] = "application/voucher-cose+cbor"
+
+      stub_request(:post, "https://highway-test.example.com:9443/.well-known/est/requestvoucher").
+        with(headers: {
+               'Accept'=>['*/*', 'multipart/mixed'],
+               'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+               'Content-Type'=>'application/voucher-cose+cbor',
+               'Host'=>'highway-test.example.com:9443',
+             }).
+        to_return(status: 404)
+
+      $FAKED_TEMPORARY_KEY = temporary_key
+      post '/e/rv', :params => body, :headers => env
+
+      expect(response).to have_http_status(404)
+
+      expect(assigns(:voucherreq)).to_not be_nil
+      expect(assigns(:voucherreq).status).to_not be_nil
+      byebug
+      expect(assigns(:voucherreq).status["masa_voucher_error"]).to_not be_nil
+    end
+
     # in order to ignore signatures in CBOR objects, look for "}}, h'" at the
     # end of the diag output, and nil it out, and also take care of time.
     def cmp_vch_file_nosig(token, basename)
