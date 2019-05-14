5d9c053720587cf2728f2106351bcaea46f43512
Michael Richardson <mcr@sandelman.ca>
2019-05-14 13:10:14 -0400

    log certificate of connecting clients, and note if they are administrators

--- app/controllers/secure_gateway_controller.rb
+++ app/controllers/secure_gateway_controller.rb
@@ -4,12 +4,21 @@ class SecureGatewayController < ApplicationController
 
   #protected
   def ssl_authenticator_lookup
+    clientname = "unknown"
     @peer_cert = request.env["SSL_CLIENT_CERT"] || request.env["rack.peer_cert"]
     if @peer_cert
       @clientcert =  OpenSSL::X509::Certificate.new(@peer_cert)
+      if @clientcert
+        clientname = sprintf("DN: %s, id: unknown", @clientcert.subject.to_s)
+      end
 
       @administrator = Administrator.find_by_cert(@clientcert)
+      if @administrator
+        clientname = sprintf("DN: %s, id: %d", @clientcert.subject.to_s, @administrator.id)
+      end
     end
+
+    logger.info "Connection from #{clientname}"
   end
 
   def ssl_login
