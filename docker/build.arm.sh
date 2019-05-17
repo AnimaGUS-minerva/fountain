e1558590182be2c0c2323685946d95263a68eaa2
Michael Richardson <mcr@sandelman.ca>
2019-05-17 00:47:19 +0000

    build 32-bit arm container

--- docker/build.arm.sh
+++ docker/build.arm.sh
@@ -2,6 +2,6 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/shg_mud_supervisor:margarita -f docker/Dockerfile.arm .
+docker build -t mcr314/shg_mud_supervisor:margarita32 -f docker/Dockerfile.arm .
 
-docker run -u root -v $(pwd)/../VMs/:/backup mcr314/shg_mud_supervisor:margarita tar -c -z --exclude backup --exclude proc --exclude dev --exclude sys -f /backup/shg_mud_supervisor-$(date +%Y%m%d)-margarita.tgz /
+docker run -u root -v $(pwd)/../VMs/:/backup mcr314/shg_mud_supervisor:margarita32 tar -c -z --exclude backup --exclude proc --exclude dev --exclude sys -f /backup/shg_mud_supervisor-$(date +%Y%m%d)-margarita32.tgz /
--- docker/builder.arm.sh
+++ docker/builder.arm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd $(dirname $0)/..
-docker build -t mcr314/shg_mud_builder:margarita -f docker/Dockerfile.builder.arm .
+docker build -t mcr314/shg_mud_builder:margarita32 -f docker/Dockerfile.builder.arm .
 docker/extract-lockfile.sh
 
 
