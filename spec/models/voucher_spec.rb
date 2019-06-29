fcd2c4a1e261833f414e0d54f5f5458aa3caa9d2
Michael Richardson <mcr@sandelman.ca>
2019-06-29 18:58:21 -0400

    update reference to thing1

--- spec/models/voucher_spec.rb
+++ spec/models/voucher_spec.rb
@@ -53,7 +53,7 @@ RSpec.describe Voucher, type: :model do
     it "should load a constrained voucher representation, and create a database object for it" do
       voucher_binary = IO::read(File.join("spec","files","voucher_jada123456789.vch"))
       v1 = CoseVoucher.from_voucher(nil, :cose, voucher_binary)
-      expect(v1.device).to eq(devices(:n3))
+      expect(v1.device).to eq(devices(:thing1))
     end
 
     it "should get a voucher format error on empty voucher object" do
