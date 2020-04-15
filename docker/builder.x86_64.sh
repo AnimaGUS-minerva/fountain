52365bb7662c8f97266d1995c7618a00826976cc
Michael Richardson <mcr@sandelman.ca>
2020-04-15 14:42:21 -0400

    rename Dockerfiles for minerva environment so that it is more obvious how they are connected

--- docker/builder.x86_64.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-cd $(dirname $0)/..
-docker build -t mcr314/brski_fountain_builder:v202004 -f docker/Dockerfile.builder.x86_64 .
-docker push mcr314/brski_fountain_builder:v202004
---
+++ docker/minerva.builder.x86_64.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+cd $(dirname $0)/..
+docker build -t mcr314/minerva_builder:v202004 -f docker/minerva.Dockerfile.x86_64 .
+docker push mcr314/minerva_builder:v202004
