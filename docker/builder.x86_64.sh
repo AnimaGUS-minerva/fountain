2c781324e396fc459da4e3ecf60b44a5e31e197f
Michael Richardson <mcr@sandelman.ca>
2020-04-15 14:31:52 -0400

    updated/created builder environment

--- docker/builder.x86_64.sh
+++ docker/builder.x86_64.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+set -e
 cd $(dirname $0)/..
-docker build -t mcr314/brski_fountain_builder:ietf104 -f docker/Dockerfile.builder.x86_64 .
-
+docker build -t mcr314/brski_fountain_builder:v202004 -f docker/Dockerfile.builder.x86_64 .
+docker push mcr314/brski_fountain_builder:v202004
