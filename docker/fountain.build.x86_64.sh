2a4cde9478e8361ddf664f6feb6a35294d7624a0
Michael Richardson <mcr@sandelman.ca>
2020-07-17 16:30:07 -0400

    bump minerva_builder base version

--- docker/fountain.build.x86_64.sh
+++ docker/fountain.build.x86_64.sh
@@ -2,6 +2,6 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/minerva_fountain:v202006 -f docker/fountain.Dockerfile.x86_64 .
-docker push mcr314/minerva_fountain:v202006
+docker build -t mcr314/minerva_fountain:v202007 -f docker/fountain.Dockerfile.x86_64 .
+docker push mcr314/minerva_fountain:v202007
 
