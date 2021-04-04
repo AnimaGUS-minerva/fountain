7de46d731b7fd59ed7cd6c94df49cf4d997a3f76
Michael Richardson <mcr@sandelman.ca>
2021-04-04 17:38:43 -0400

    updated Gemfile.lock for docker image

--- docker/fountain.build.x86_64.sh
+++ docker/fountain.build.x86_64.sh
@@ -2,6 +2,6 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/minerva_fountain:v202007 -f docker/fountain.Dockerfile.x86_64 .
-docker push mcr314/minerva_fountain:v202007
+docker build -t mcr314/minerva_fountain:v202104 -f docker/fountain.Dockerfile.x86_64 .
+docker push mcr314/minerva_fountain:v202104
 
