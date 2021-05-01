4d1d4c51aef981c63f42ff68f7e64deaea2e0619
Michael Richardson <mcr@sandelman.ca>
2021-05-01 17:28:20 -0400

    updated build tag

--- docker/fountain.build.x86_64.sh
+++ docker/fountain.build.x86_64.sh
@@ -2,6 +2,6 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/minerva_fountain:v202104 -f docker/fountain.Dockerfile.x86_64 .
-docker push mcr314/minerva_fountain:v202104
+docker build -t mcr314/minerva_fountain:v202105 -f docker/fountain.Dockerfile.x86_64 .
+docker push mcr314/minerva_fountain:v202105
 
