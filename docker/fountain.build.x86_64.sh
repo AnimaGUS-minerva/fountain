38ffc67561eca4e73c2da7d8523b9f0bc4152d9b
Michael Richardson <mcr@sandelman.ca>
2021-08-05 18:11:11 -0400

    updates for static linked openssl

--- docker/fountain.build.x86_64.sh
+++ docker/fountain.build.x86_64.sh
@@ -2,6 +2,6 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/minerva_fountain:v202107 -f docker/fountain.Dockerfile.x86_64 .
-docker push mcr314/minerva_fountain:v202107
+docker build -t mcr314/minerva_fountain:v202108 -f docker/fountain.Dockerfile.x86_64 .
+docker push mcr314/minerva_fountain:v202108
 
