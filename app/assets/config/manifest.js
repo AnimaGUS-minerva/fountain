b4b190feb51ed90edb07e682cadfad065928e0ac
Michael Richardson <mcr@sandelman.ca>
2022-05-31 15:28:51 -0400

    add missing default asset configuration file compile it in the docker instructions

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Fountain really has no assets, it's all APIs for now.
+//
+//= require turbolinks
+//= require_tree .
