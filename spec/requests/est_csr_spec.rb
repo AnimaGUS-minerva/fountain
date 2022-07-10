c2fe09faf9487b0f0783597d42480aeebf0a272b
Michael Richardson <mcr@sandelman.ca>
2022-07-10 19:40:49 -0400

    added test case for old, wrong way to generate CSR Attributes with values

--- spec/requests/est_csr_spec.rb
+++ spec/requests/est_csr_spec.rb
@@ -68,6 +68,22 @@ RSpec.describe "Est", type: :request do
       expect(response).to have_http_status(200)
     end
 
+    it "should return an ACP nodeName in the attributes" do
+      env = Hash.new
+      env["SSL_CLIENT_CERT"] = highwaytest_clientcert_f20001
+      get "/.well-known/est/csrattributes", :headers => env
+
+      # given that f20001 cert is used, it should assign device to
+      # jadaf20001 device
+      expect(assigns(:device)).to eq(devices(:jadaf20001))
+      expect(response).to have_http_status(200)
+
+      asn1 = OpenSSL::ASN1.decode(response.body)
+      expect(asn1.value[0].value[0].value).to eq("subjectAltName")
+      expect(asn1.value[0].value[1].
+               value[0].value[0].value[0].value).to eq("rfc8994+fd739fc23c3440112233445500000000+@acp.example.com")
+    end
+
     it "should fail to be returned with acertificate with untrusted connection" do
       env = Hash.new
       env["SSL_CLIENT_CERT"] = clientcert
