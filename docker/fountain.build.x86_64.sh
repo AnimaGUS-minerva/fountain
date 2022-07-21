e7ef7aa96ca9682ae91233ad98361b6d0960ae1d
Michael Richardson <mcr@sandelman.ca>
2022-07-20 21:53:01 -0400

    bump version after dependabot updates

--- docker/fountain.build.x86_64.sh
+++ docker/fountain.build.x86_64.sh
@@ -2,6 +2,6 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/minerva_fountain:v202206 -f docker/fountain.Dockerfile.x86_64 .
-docker push mcr314/minerva_fountain:v202206
+docker build -t mcr314/minerva_fountain:v202207 -f docker/fountain.Dockerfile.x86_64 .
+docker push mcr314/minerva_fountain:v202207
 
