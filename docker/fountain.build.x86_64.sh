2a5c7a51c96c35b9f42afab54349a3360e83fb0d
Michael Richardson <mcr@sandelman.ca>
2022-08-09 06:06:22 -0400

    updated docker build with passenger 6.x, new tags

--- docker/fountain.build.x86_64.sh
+++ docker/fountain.build.x86_64.sh
@@ -2,6 +2,8 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t mcr314/minerva_fountain:v202207 -f docker/fountain.Dockerfile.x86_64 .
-docker push mcr314/minerva_fountain:v202207
+docker build -t dockerhub.gatineau.credil.org/minerva/minerva_fountain:v202208 -f docker/fountain.Dockerfile.x86_64 .
+docker push dockerhub.gatineau.credil.org/minerva/minerva_fountain:v202208
 
+#docker tag rails-alpine:7.0.3 dockerhub.gatineau.credil.org/library/rails-alpine:7.0.3
+#docker push dockerhub.gatineau.credil.org/library/rails-alpine:7.0.3
