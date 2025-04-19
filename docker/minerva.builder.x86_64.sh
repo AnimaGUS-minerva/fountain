180140dd764c8caa84474862b679411ecda17081
Michael Richardson <mcr@sandelman.ca>
2025-04-19 18:24:01 -0400

    feat: updates to minerva base image, upgraded passenger, work around conflicting rackup

--- docker/minerva.builder.x86_64.sh
+++ docker/minerva.builder.x86_64.sh
@@ -2,5 +2,5 @@
 
 set -e
 cd $(dirname $0)/..
-docker build -t dockerhub.gatineau.credil.org/minerva/minerva_builder:v202304 -f docker/minerva.Dockerfile.x86_64 .
-docker push dockerhub.gatineau.credil.org/minerva/minerva_builder:v202304
+docker build -t dockerhub.gatineau.credil.org/minerva/minerva_builder:v202504 -f docker/minerva.Dockerfile.x86_64 .
+docker push dockerhub.gatineau.credil.org/minerva/minerva_builder:v202504
