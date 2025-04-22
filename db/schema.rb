e6e81848b5e67abfd4a341e41b03b4c9898ab9f2
Michael Richardson <mcr@sandelman.ca>
2025-04-22 09:15:42 -0400

    feat: updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2022_06_06_201327) do
+ActiveRecord::Schema[8.0].define(version: 2022_06_06_201327) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "administrators", force: :cascade do |t|
     t.text "name"
@@ -128,5 +128,4 @@ ActiveRecord::Schema[7.1].define(version: 2022_06_06_201327) do
     t.string "type", default: "CmsVoucher"
     t.binary "encoded_details"
   end
-
 end
